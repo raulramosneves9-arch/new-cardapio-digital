1b4983126740a74b812654a2b4295ae50ddc5542
Raul Ramos <raulramosneves9@gmail.com>
2026-04-16 17:03:28 -0300

    feat: estrutura inicial da tela de menu

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,17 +1,26 @@
 <script>
-import MenuForm from '@/components/MenuForm.vue'
-import { fetchSuggestions } from '@/services/api'
-import { NOTIFY, ALL_CATEGORY } from '@/constants'
+import { NOTIFICATION_TYPES, ALL_CATEGORY } from '@/constants'
 
 export default {
     name: 'MenuView',
+    data() {
+        return {
+            ALL_CATEGORY
+        }
+    }
 }
 </script>
 
 <template>
     <div class="menu-view">
         <h1>Menu</h1>
+        <p>Categoria padrão: {{ ALL_CATEGORY }}</p>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.menu-view {
+    padding: 20px;
+    color: white;
+}
+</style>
