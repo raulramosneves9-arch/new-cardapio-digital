908110d26c996fc03ee9a7a522c2c191496539d5
Raul Ramos <raulramosneves9@gmail.com>
2026-04-16 16:39:50 -0300

    theme ajustado, navbar e homeview adicionado template e css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,22 @@
 <template>
-    <nav class="navbar">
-        <!-- Navigation bar component -->
+    <nav class="navbar navbar-expand-lg custom-navbar">
+        <div class="container">
+
+            <a class="navbar-brand text-light fw-bold" href="#">
+                🍽️ Cardápio Digital
+            </a>
+
+            <div class="collapse navbar-collapse justify-content-center">
+                <ul class="navbar-nav gap-4">
+                    <li class="nav-item"><a class="nav-link" href="#">Cardápio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Carrinho</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Sobre</a></li>
+                </ul>
+            </div>
+
+            <button class="btn btn-login">Entrar</button>
+
+        </div>
     </nav>
 </template>
 
