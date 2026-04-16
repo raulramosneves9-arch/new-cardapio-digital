c3ebd56e85ef1fc7f186ea940ae003069b66ab47
Raul Ramos <raulramosneves9@gmail.com>
2026-04-16 17:03:20 -0300

    feat: adiciona formatação de moeda BRL

--- src/utils/format.js
+++ src/utils/format.js
@@ -31,3 +31,7 @@ export const truncateText = (text, maxLength = 50) => {
 export const capitalizeFirstLetter = (text) => {
     return text.charAt(0).toUpperCase() + text.slice(1)
 }
+
+export const formatBRL = (value) => {
+    return formatCurrency(value, 'pt-BR', 'BRL')
+}
